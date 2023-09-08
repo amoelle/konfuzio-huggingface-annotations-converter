133754e5bdef38f9ed19b9ccd8c434a20c714b66
christopher-czaban <cpczaban@gmail.com>
2023-09-08 16:30:32 +0200

    Finalisierung erste Version

--- annotations_converter/main.py
+++ annotations_converter/main.py
@@ -1,9 +1,10 @@
 import os
 import json
+import pandas as pd
 from utils import (process_all_documents, save_to_json, load_from_json, draw_boxes)
 
 
-def draw_boxes_from_config():
+def draw_boxes_from_user_input():
     """
     Draw bounding boxes based on the configuration provided by the user.
     """
@@ -11,78 +12,132 @@ def draw_boxes_from_config():
     project_id = input("Please enter the project ID: ")
     document_id = input("Please enter the document ID: ")
     page_number = input("Please enter the page number: ")
+
+    # Prompt the user for the type of bounding box2
+    bbox_type = input("Choose the type of bounding box to draw ( bbox / merged_bbboxes / merged_bbboxes_by_line ): ")
+    while bbox_type not in ['bbox', 'merged_bbboxes', 'merged_bbboxes_by_line']:
+        print("Invalid choice. Please select a valid bounding box type.")
+        bbox_type = input("Choose the type of bounding box to draw ( bbox / merged_bbboxes / merged_bbboxes_by_line ): ")
     
-    file_path = f"data_{project_id}/documents/{document_id}/page_{page_number}.png"
-    
+    # Get the directory where the script is located
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    # Move one directory up from the script's directory
+    parent_dir = os.path.dirname(script_dir)
+
+    konfuzio_document_dir = f"data_{project_id}/documents/{document_id}/"
+   
+ 
+    # Path to stored data
+    data_dir = "data"
+    file_name = f"project_{project_id}_docs.json"
+    # Construct the file path relative to the script's directory
+    dir_path = os.path.join(parent_dir, data_dir) 
+    file_path = os.path.join(dir_path, file_name) 
+
+    # Load the corresponding dataframe
+    print(f"Trying to load {file_path} ...")
     if not os.path.exists(file_path):
         print(f"Error: File {file_path} does not exist. Please ensure you've entered the correct details.")
         return
-    
-    # Load the corresponding dataframe
-    json_data = load_from_json(f'konfuzio_converted_project_{project_id}_dataset.json')
-    
-    doc_key = f"document_{document_id}"
+    json_data = load_from_json(file_path)
+
+    doc_key = f"{document_id}"
     if doc_key not in json_data:
-        print(f"Error: Document ID {document_id} data not found in stored datasets.")
+        print(f"Error: Document ID {document_id} not found in stored datasets.")
         return
     
+    while len(json_data[doc_key]['pages']) < int(page_number):
+        page_number = input("Specified page number out of range. Please enter a page number smaller or equal {}:".format(len(json_data[doc_key]['pages'])))
+        
+    image_name = f"page_{page_number}.png"
+    # Construct the file path relative to the script's directory
+    image_path = os.path.join(parent_dir, konfuzio_document_dir, image_name) 
+    if not os.path.exists(image_path):
+        print(f"Error: Image {file_path} does not exist. Please ensure it was saved before.")
+        return
+    
+    image_short_path = os.path.join(konfuzio_document_dir, image_name) 
     dataframe_page = json_data[doc_key]['pages'][int(page_number) - 1]
     df = pd.DataFrame(dataframe_page)
-    
+    print(df.head())
+
     # Draw boxes on the image
-    image = draw_boxes(file_path, df, 'bbox')  # or 'merged_bbboxes' or 'merged_bbboxes_by_line' based on your requirement
-    image.show()
+    image = draw_boxes(image_path, image_short_path, df, bbox_type)  # or 'merged_bbboxes' or 'merged_bbboxes_by_line' based on your requirement
+    #image.show()
+    image_save_path = os.path.join(parent_dir, data_dir, "output.png")
+    image.save(image_save_path)
+
+    return
+1
+def process_documents_from_config(config):
+    """
+    Handle processing of documents based on the provided configuration.
+    """
+    
+    # Extract configuration details
+    project_id = config["project_id"]
+    document_types = config["document_types"]
+    allowed_types = ["train", "test", "no_status", "preparation"]
+    max_nr_docs_per_type = config['max_nr_docs_per_type']
+
+    # Check for invalid document types
+    invalid_types = [doc_type for doc_type in document_types if doc_type not in allowed_types]
+
+    if invalid_types:
+        print(f"Error: Invalid document types {invalid_types} found in config.json. Allowed types are {allowed_types}.")
+        exit(1)
+    
+    print(f"Processing project {project_id} with document types {document_types}...")
+    # Process the documents
+    tabular_document_datasets_dict = process_all_documents(project_id, document_types, max_nr_docs_per_type)
+    
+    #document_types_string = "_".join(document_types)
+    # Get the directory where the script is located
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    # Move one directory up from the script's directory
+    parent_dir = os.path.dirname(script_dir)
+    data_dir = "data"
+    file_name = f"project_{project_id}_docs.json"
+    # Construct the file path relative to the script's directory
+    dir_path = os.path.join(parent_dir, data_dir) 
+    file_path = os.path.join(dir_path, file_name) 
+
+    if not os.path.exists(dir_path):
+        print(f"Error: Directory {dir_path} does not exist. Please ensure you've entered the correct details.")
+    else:
+        print(f"Saving {file_path} ...")
+        # Store the processed data
+        save_to_json(tabular_document_datasets_dict, file_path)
 
     return
 
-
 def main():
+     
     while True:
         print("\nChoose an option:")
         print("1. Load and Store Data from Konfuzio")
         print("2. Draw Visualized Data")
         print("3. Exit")
         choice = input("> ")
-
+    
         if choice == "1":
+
+            # Get the directory where the script is located
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            file_path = os.path.join(script_dir, f"./config.json") 
+
             # Load configuration
-            with open('config.json', 'r') as file:
+            with open(file_path, 'r') as file:
                 config = json.load(file)
-            project_id = config["project_id"]
-            document_types = config["document_types"]
 
-            allowed_types = ["train", "test", "no_status", "preparation"]
-
-            # Check for invalid document types
-            invalid_types = [doc_type for doc_type in document_types if doc_type not in allowed_types]
+            # Call the process_documents_from_config function
+            process_documents_from_config(config)
+            break
         
-            if invalid_types:
-                print(f"Error: Invalid document types {invalid_types} found in config.json. Allowed types are {allowed_types}.")
-                exit(1)
-
-            project = Project(id_=project_id, update=True, strict_data_validation=False)
-
-            # Call the process_and_store_documents function to handle further steps
-            process_and_store_documents(project_number, document_types)
-
-            print(f"Processing project {project_number} with document types {document_types}...")
-
         elif choice == "2":
-            project_id = input("Enter Project ID: ")
-            document_id = input("Enter Document ID: ")
-            page_number = input("Enter Page Number: ")
-
-            # Construct the expected file path based on user input
-            file_path = os.path.join('../', f'data_{project_id}', 'documents', f'{document_id}', f'page_{page_number}.png')
-
-            if os.path.exists(file_path):
-                print(f"Drawing boxes for file {file_path}...")
-                # Assuming you have a dataframe corresponding to this file. 
-                # You might want to adjust how you fetch this dataframe.
-                df = ... 
-                draw_boxes(file_path, df, 'bbox')
-            else:
-                print(f"File {file_path} not found!")
+            
+            draw_boxes_from_user_input()
+            break
 
         elif choice == "3":
             print("Exiting the application.")
@@ -91,5 +146,7 @@ def main():
         else:
             print("Invalid choice. Please select again.")
 
+
 if __name__ == "__main__":
+    
     main()
--- annotations_converter/utils.py
+++ annotations_converter/utils.py
@@ -6,6 +6,8 @@ from konfuzio_sdk.tokenizer.regex import WhitespaceTokenizer as KWT
 from nltk.tokenize import word_tokenize, NLTKWordTokenizer
 from nltk.tokenize import WhitespaceTokenizer as WT
 from PIL import Image, ImageDraw, ImageFont
+from copy import deepcopy
+import json
 
 
 def get_word_bboxes(document):
@@ -18,40 +20,100 @@ def get_word_bboxes(document):
     return tk_document
 
 
-def process_all_documents(documents):
-    """
-    Process all documents and organize the results in a structured dictionary.
+def process_annotated_document(document):
+    dataset_column_dictionary = {
+        'annotation_id': [],
+        'page_index': [],
+        'line_number': [],
+        'top': [],
+        'bottom': [],
+        'x0': [],
+        'y0': [],
+        'x1': [],
+        'y1': [],
+        'label': [],
+        'label_set': [],
+        'start_offset': [],
+        'end_offset': [],
+        'offset_string': [],
+        'file_name': [],
+        'file_path': [],
+    }
 
-    Parameters:
-    - documents: List of documents to be processed.
+    annotations = document.get_annotations()
+    file_paths_array = []
+    for idx, _ in enumerate(annotations):
+        
+        if annotations[idx].is_correct != True:
+            continue
+        
+        nr_bboxes_in_annotations = len(annotations[idx].bboxes)
 
-    Returns:
-    - tabular_document_datasets_dict: A dictionary where the key is the document's name and 
-      the value is another dictionary containing 'pages' and 'document_text'.
-    """
+        annotation_id = annotations[idx].id_
+
+        for bnr in range(nr_bboxes_in_annotations):
+
+            bbox = annotations[idx].bboxes[bnr]
+
+            page = document.pages()[bbox['page_index']]
+            line_number = bbox['line_number']
+            up_factor_x = page.get_image().width / page.width  # image > page, thus up_factor_x > 1
+            up_factor_y = page.get_image().height / page.height # image > page, thus up_factor_y > 1
+
+            page_index = bbox['page_index'] + 1
+            top = bbox['top']
+            bottom = bbox['bottom']
+
+            x0 = bbox['x0'] * up_factor_x
+            y0 = bbox['top'] * up_factor_y
+            x1 = bbox['x1'] * up_factor_x
+            y1 = bbox['bottom'] * up_factor_y
+
+            label = annotations[idx].label.name_clean
+            label_set = annotations[idx].label_set.name_clean
     
-    tabular_document_datasets_dict = {}
+            start_offset = bbox['start_offset']
+            end_offset = bbox['end_offset']
+            offset_string = bbox['offset_string']
+            file_name = "page_" + str(page_index) + ".png"
 
-    for document in documents:
-        # Get dataframes from the document
-        no_label_annotations_df, label_annotations_df = get_dataframes_from_doc(document)
-        
-        # Transfer annotations and merge bounding boxes
-        full_df = transfer_annotations(no_label_annotations_df, label_annotations_df)
-        full_df = merge_bounding_boxes(full_df)
-        
-        # Split up full_df into chunks corresponding to the pages
-        pages_list = []
-        for _, group in full_df.groupby('file_name'):
-            pages_list.append(group)
-        
-        # Store the data in the main dictionary
-        tabular_document_datasets_dict[document.name] = {
-            'pages': pages_list,
-            'document_text': document.text
-        }
+            project_id = document.project.id_
 
-    return tabular_document_datasets_dict
+            # Get the directory where the script is located
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            parent_dir = os.path.dirname(script_dir)
+            document_id = str(annotations[idx].document.copy_of_id)
+            konfuzio_document_dir = f"data_{project_id}/documents/{document_id}/"
+            full_file_path = os.path.join(parent_dir, konfuzio_document_dir, file_name) 
+            file_path = os.path.join(konfuzio_document_dir, file_name) 
+            file_paths_array.append(file_path)
+
+            dataset_column_dictionary['annotation_id'].append(annotation_id)
+            dataset_column_dictionary['page_index'].append(page_index)
+            dataset_column_dictionary['line_number'].append(line_number)
+
+            dataset_column_dictionary['top'].append(top)
+            dataset_column_dictionary['bottom'].append(bottom)
+            dataset_column_dictionary['x0'].append(x0)
+            dataset_column_dictionary['y0'].append(y0)
+            dataset_column_dictionary['x1'].append(x1)
+            dataset_column_dictionary['y1'].append(y1)
+
+
+            dataset_column_dictionary['label'].append(label)
+            dataset_column_dictionary['label_set'].append(label_set)
+            dataset_column_dictionary['start_offset'].append(start_offset)
+            dataset_column_dictionary['end_offset'].append(end_offset)
+            dataset_column_dictionary['offset_string'].append(offset_string)
+            dataset_column_dictionary['file_name'].append(file_name)
+            dataset_column_dictionary['file_path'].append(file_path)
+
+            # if os.path.exists(file_path):
+            #     dataset_column_dictionary['file_path'].append(file_path)
+            # else:
+            #     dataset_column_dictionary['file_path'].append("NA")
+
+    return dataset_column_dictionary, file_paths_array
 
 
 def process_word_bboxes_document(document):
@@ -71,7 +133,7 @@ def process_word_bboxes_document(document):
         'start_offset': [],
         'end_offset': [],
         'file_name': [],
-        'file_path': []
+        'file_path': [],
     }
 
     annotations = document.get_annotations()
@@ -101,7 +163,16 @@ def process_word_bboxes_document(document):
             end_offset = bbox['end_offset']
             offset_string = bbox['offset_string']
             file_name = "page_" + str(page_index) + ".png"
-            file_path =  os.getcwd() + '/' + 'data_' + str(project_id) + '/documents/' + str(annotations[idx].document.copy_of_id) + "/" + file_name
+
+            project_id = document.project.id_
+
+            # Get the directory where the script is located
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            parent_dir = os.path.dirname(script_dir)
+            document_id = str(annotations[idx].document.copy_of_id)
+            konfuzio_document_dir = f"data_{project_id}/documents/{document_id}/"
+            full_file_path = os.path.join(parent_dir, konfuzio_document_dir, file_name) 
+            file_path = os.path.join(konfuzio_document_dir, file_name)
             file_paths_array.append(file_path)
 
             dataset_column_dictionary['page_index'].append(page_index)
@@ -118,12 +189,28 @@ def process_word_bboxes_document(document):
             dataset_column_dictionary['y1'].append(y1)
             dataset_column_dictionary['line_number'].append(line_number)
             dataset_column_dictionary['file_name'].append(file_name)
-            if os.path.exists(file_path):
-                dataset_column_dictionary['file_path'].append(file_path)
-            else:
-                dataset_column_dictionary['file_path'].append("NA")
+            dataset_column_dictionary['file_path'].append(file_path)
+            
+            # if os.path.exists(file_path):
+            #     dataset_column_dictionary['file_path'].append(file_path)
+            # else:
+            #     dataset_column_dictionary['file_path'].append("NA")
 
     return dataset_column_dictionary, file_paths_array
+
+
+
+def get_dataframes_from_doc(document: konfuzio_sdk.data.Document) -> konfuzio_sdk.data.Document:
+    
+    doc_with_word_level_bboxes = get_word_bboxes(document)
+    word_boxes_dataset_column_dictionary, _ = process_word_bboxes_document(doc_with_word_level_bboxes)
+    no_label_annotations_df = pd.DataFrame(word_boxes_dataset_column_dictionary)
+
+    dataset_column_dictionary, _ = process_annotated_document(document)
+    label_annotations_df = pd.DataFrame(dataset_column_dictionary)
+
+    return no_label_annotations_df, label_annotations_df
+
 
 
 def transfer_annotations(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
@@ -151,9 +238,124 @@ def transfer_annotations(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return full_df
 
 
-from PIL import Image, ImageDraw, ImageFont
 
-def draw_boxes(image_path: str, dataframe: pd.DataFrame, bbox_column: str) -> Image.Image:
+def merge_bounding_boxes(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Merge bounding boxes based on 'annotation_id' and 'annotation_id' + 'line_number'.
+    
+    Parameters:
+    - dataframe: DataFrame containing the 'bbox', 'annotation_id', and 'line_number' columns
+    
+    Returns:
+    - DataFrame with the added 'merged_bbboxes' and 'merged_bbboxes_by_line' columns
+    """
+    
+    # --- First Round: Merge based on annotation_id only ---
+    
+    def merge_bboxes(group):
+        """
+        Merge bounding boxes for a group of rows.
+        """
+        # Ensure that the 'bbox' column contains strings
+        bboxes_str = group['bbox'].astype(str)
+        
+        # Extract bounding boxes from the 'bbox' column
+        bboxes = bboxes_str.str.strip("[]").str.split(",", expand=True).astype(float)
+        
+        # Compute the merged bounding box
+        merged_bbox = [
+            int(bboxes[0].min()),  # x0
+            int(bboxes[1].min()),  # y0
+            int(bboxes[2].max()),  # x1
+            int(bboxes[3].max())   # y1
+        ]
+        
+        return merged_bbox
+    
+    # Group by 'annotation_id' and apply the merge_bboxes function
+    merged_bboxes_dict = dataframe.groupby('annotation_id').apply(merge_bboxes).to_dict()
+
+    # Map the merged bounding boxes to the original dataframe using 'annotation_id' as the key
+    dataframe['merged_bbboxes'] = dataframe['annotation_id'].map(merged_bboxes_dict)
+    
+    # For rows without a related annotation, copy the bounding box from 'bbox'
+    dataframe['merged_bbboxes'].fillna(dataframe['bbox'], inplace=True)
+    
+    # --- Second Round: Merge based on annotation_id and line_number ---
+    
+    # Group by 'annotation_id' and 'line_number' and apply the merge_bboxes function
+    key = dataframe['annotation_id'].astype(str) + "_" + dataframe['line_number'].astype(str)
+    merged_bboxes_by_line_dict = dataframe.groupby(['annotation_id', 'line_number']).apply(merge_bboxes).to_dict()
+
+    # Convert multi-index dict keys to concatenated string keys with underscore separator
+    merged_bboxes_by_line_dict = {str(k[0]) + "_" + str(k[1]): v for k, v in merged_bboxes_by_line_dict.items()}
+
+    # Map the merged bounding boxes to a new column in the dataframe using the unique key
+    dataframe['merged_bbboxes_by_line'] = key.map(merged_bboxes_by_line_dict)
+    
+    # For rows without a merged bounding box based on both annotation_id and line_number, copy the bounding box from 'bbox'
+    dataframe['merged_bbboxes_by_line'].fillna(dataframe['bbox'], inplace=True)
+    
+    return dataframe
+
+
+
+def process_all_documents(project_id, document_types, max_nr_docs_per_type):
+    """
+    Process all documents and organize the results in a structured dictionary.
+
+    Parameters:
+    - documents: List of documents to be processed.
+
+    Returns:
+    - tabular_document_datasets_dict: A dictionary where the key is the document's name and 
+      the value is another dictionary containing 'pages' and 'document_text'.
+    """
+    
+    project = Project(id_=project_id, update=True, strict_data_validation=False)
+
+    document_types_dict = {
+        'train': project.documents,
+        'test': project.test_documents,
+        'no_status': project.no_status_documents,
+        'preparation': project.preparation_documents
+        }
+    
+    documents = []
+    for doc_type in document_types:
+        documents.extend(document_types_dict[doc_type][:max_nr_docs_per_type])
+
+    tabular_document_datasets_dict = {}
+
+    for document in documents:
+        print("Processing document {} ...".format(document.name))
+        
+        # Get dataframes from the document
+        no_label_annotations_df, label_annotations_df = get_dataframes_from_doc(document)
+
+        # Transfer annotations and merge bounding boxes
+        full_df = transfer_annotations(no_label_annotations_df, label_annotations_df)
+        full_df = merge_bounding_boxes(full_df)
+        
+        # Split up full_df into chunks corresponding to the pages
+        pages_list = []
+        for _, group in full_df.groupby('file_name'):
+            pages_list.append(group)
+        
+        # Store the data in the main dictionary
+        tabular_document_datasets_dict[document.id_] = {
+            'project_id': project_id,
+            'document_name': document.name,
+            'document_status': document.status,
+            'document_text': document.text,
+            'pages': pages_list,
+        }
+
+    return tabular_document_datasets_dict
+
+
+
+def draw_boxes(image_path: str, image_short_path: str, dataframe: pd.DataFrame, bbox_column: str) -> Image.Image:
     """
     Draw bounding boxes and their labels on the image.
 
@@ -176,7 +378,11 @@ def draw_boxes(image_path: str, dataframe: pd.DataFrame, bbox_column: str) -> Im
     draw = ImageDraw.Draw(image)
     font = ImageFont.load_default()
 
-    dataframe = dataframe[dataframe['file_path'] == image_path]
+    print("image_path: ", image_path)
+    print("image_short_path: ", image_short_path)
+    
+    dataframe = dataframe[dataframe['file_path'] == image_short_path]
+    print(dataframe.head())
 
     for _, row in dataframe.iterrows():
         # Extract bounding box and label from the dataframe
@@ -211,8 +417,11 @@ def save_to_json(data_dict, file_path):
     for doc_key, doc_value in data_dict.items():
         pages_list_serializable = [page_df.to_dict(orient='records') for page_df in doc_value['pages']]
         serializable_dict[doc_key] = {
+            'project_id': doc_value['project_id'],
+            'document_name': doc_value['document_name'],
+            'document_status': doc_value['document_status'],
+            'document_text': doc_value['document_text'],
             'pages': pages_list_serializable,
-            'document_text': doc_value['document_text']
         }
 
     # Save the serializable dictionary to a JSON file
@@ -239,8 +448,11 @@ def load_from_json(file_path):
     for doc_key, doc_value in loaded_dict.items():
         pages_list_df = [pd.DataFrame(page_data) for page_data in doc_value['pages']]
         dataframes_dict[doc_key] = {
+            'project_id': doc_value['project_id'],
+            'document_name': doc_value['document_name'],
+            'document_status': doc_value['document_status'],
+            'document_text': doc_value['document_text'],
             'pages': pages_list_df,
-            'document_text': doc_value['document_text']
         }
 
     return dataframes_dict
